60f8b20817464e2293a7a9d795d45acd1600c48b
sonu vishwakarma <sonuvishwa021@gmail.com>
2023-09-22 21:58:04 +0530

    Delete Project (telecom dataset)/Project( Churn prediction) directory

--- Project (telecom dataset)/Project( Churn prediction)/basicConcepts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Welcome, name = 'Welcome'),
-    path('user', views.User, name = 'User')
-]
--- Project (telecom dataset)/Project( Churn prediction)/basicConcepts/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def Welcome(request):
-    return render(request, 'index.html')
-
-def User(request):
-    username = request.GET['username']
-    return render(request, 'user.html', {'name':username})
